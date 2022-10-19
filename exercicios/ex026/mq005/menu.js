8a1292918f4b1965af17138a4516ee5477a52a89
LucasDLeandro <LucasDLeandro@users.noreply.github.com>
2022-10-19 10:16:15 -0300

    update

---
+++ exercicios/ex026/mq005/menu.js
@@ -0,0 +1,8 @@
+const btnMobile = document.getElementById("burguer");
+
+function toggleMenu() {
+ document.getElementById("nav").classList.add('active')
+}
+
+navigation.addEventListener("click", toggleMenu);
+
